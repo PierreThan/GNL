1a343237753d1742cafe9e1d0e8d894d1a130651
than <pierrethan@hotmail.com>
2019-01-23 03:26:41 +0100

    cyvubhk

---
+++ gnl.c
@@ -0,0 +1,55 @@
+#include "get_next_line.h"
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+static int	get_line(char **line, char **str)
+{
+	unsigned long	i;
+	char			*tmp;
+
+	i = 0;
+	tmp = *str;
+	while (str[0][i] && str[0][i] != '\n')
+		i++;
+	if (str[0][i] && str[0][i] == '\n')
+	{
+		str[0][i] = '\0';
+		i++;
+	}
+	*line = ft_strdup(*str);
+	*str = ft_strdup((const char*)(*str) + i);
+		return (-1);
+	free(tmp);
+	return (1);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static char	*res = NULL;
+	long		r;
+	char		buff[BUFF_SIZE + 1];
+	char		*tmp;
+
+	tmp = NULL;
+	if (fd < 0 || !line || read(fd, buff, 0) < 0)
+		return (-1);
+	while ((r = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[r] = '\0';
+		tmp = res;
+		if (tmp)
+		{
+			if ((res != ft_strjoin(res, buff)))
+				return (-1);
+			free(tmp);
+		}
+		else
+			res = ft_strdup(buff);
+		if (ft_strchr(res, '\n'))
+			return (get_line(line, &res));
+	}
+	if (ft_strchr(res, '\n'))
+		return (get_line(line, &res));
+	return (r);
+}
